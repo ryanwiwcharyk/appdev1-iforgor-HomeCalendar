991f87a0950a1b13902e128c4400bcb86e9f9bef
Brayden Fournier <6194481@johnabbottcollege.net>
2024-02-21 11:08:01 -0500

    Added undesired files to the .gitignore.

---
+++ jacappdev1_2024_iforgor/jacappdev1_2024_iforgor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jacappdev1_2024_iforgor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
