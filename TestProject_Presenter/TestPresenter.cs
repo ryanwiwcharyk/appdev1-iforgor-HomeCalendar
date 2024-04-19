490397307e794197419d5a1d7f0360ba345aedd8
Brayden Fournier <6194481@johnabbottcollege.net>
2024-04-19 11:21:34 -0400

    Added Notes regarding presenter tests.

--- TestProject_Presenter/TestPresenter.cs
+++ TestProject_Presenter/TestPresenter.cs
@@ -8,5 +8,51 @@ namespace TestProject_Presenter
 {
     internal class TestPresenter
     {
+        //RegisterWindow 
+        //Test MainViewInterface window
+        //Test CategoryView window
+        //Test ICreateEventViewInterface
+
+
+        //NewCalendar
+        //Test creation with proper params
+        //Test to verify Home window is being displayed
+
+
+        //ExistingCalendar
+        //opening of the calendar
+        //Verify that Home window is being displayed
+
+
+        //GetUpcomingEvents
+        //no upcoming events
+        //with some upcoming events
+        //correct passing of events to mainview
+
+
+        //PopulateCategoryDropdown
+        //ensure valid population of dropdown with categories
+
+
+        //ValidateEventFormAndCreate
+        //Test each params for valid creation
+        //No details 
+        //Invalid duration
+        //No start time 
+        //No category
+        //ensure a succesful event creation
+        //ensure failed creation due to invalid params^
+
+
+        //PopulateCategoryTypesDropdown
+        //Ensure proper categroy types in dropdown.
+
+
+        //ValidateDetailsFormInputAndCreateCategory
+        //No details
+        //invalid type
+        //all valid types (4 of them iykyk)
+        //creation of category is successful
+        //ensuring the call to PopulateCategoryDropdown once created
     }
 }
