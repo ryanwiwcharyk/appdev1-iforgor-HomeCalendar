d2ba301088f6227fd37fd3856cd61a60553c9c9c
ryanwiwcharyk <124375048+ryanwiwcharyk@users.noreply.github.com>
2024-05-01 21:27:35 -0400

    Added property to get an events id in update window, and cleaned up code.

--- HomeCalendarWPF/UpdateEventWindow.xaml.cs
+++ HomeCalendarWPF/UpdateEventWindow.xaml.cs
@@ -2,17 +2,7 @@
 using HomeCalendarWPF.interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HomeCalendarWPF
 {
@@ -24,14 +14,21 @@ namespace HomeCalendarWPF
         readonly Presenter _presenter;
         private CalendarItem _calendarItemToUpdate;
 
+        #region Constructor
         public UpdateEventWindow(Presenter presenter, CalendarItem selectedCalendarItem)
         {
             InitializeComponent();
             _calendarItemToUpdate = selectedCalendarItem;
             _presenter = presenter;
             InitializeWindow();
-            
         }
+        #endregion
+
+        #region Properties
+        public int EventId { get { return _calendarItemToUpdate.EventID; } }
+        #endregion
+
+        #region Interface Implementation
 
         public void ShowPopulatedFields(string details, double duration, DateTime startDate, int hours, int minutes, Category category)
         {
@@ -43,18 +40,25 @@ namespace HomeCalendarWPF
             categoryComboBox.Text = category.Description;
         }
 
+        public void AddCategoriesToDropdown(List<Category> categories)
+        {
+            categoryComboBox.ItemsSource = categories;
+        }
+
         public void ShowErrorPopup(string message)
         {
-            MessageBox.Show(message, "Home Calendar");
+            MessageBox.Show(message, "Home Calendar", MessageBoxButton.OK, MessageBoxImage.Error);
 
         }
         public void ShowSuccessPopup(string message)
         {
-            MessageBox.Show(message, "Home Calendar");
+            MessageBox.Show(message, "Home Calendar", MessageBoxButton.OK, MessageBoxImage.Information);
             this.Close();
-
         }
 
+        #endregion
+
+        #region Private Methods
         private void InitializeWindow()
         {
             _presenter.RegisterWindow(this);
@@ -82,7 +86,7 @@ namespace HomeCalendarWPF
 
         private void BtnClick_CancelEvent(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult messageBoxResult = MessageBox.Show("You are about to cancel the update of this event", "Home Calendar", MessageBoxButton.YesNo);
+            MessageBoxResult messageBoxResult = MessageBox.Show("You are about to cancel the update of this event", "Home Calendar", MessageBoxButton.YesNo, MessageBoxImage.Warning);
             if (messageBoxResult == MessageBoxResult.Yes)
             {
                 this.Close();
@@ -91,23 +95,20 @@ namespace HomeCalendarWPF
 
         private void BtnClick_UpdateEvent(object sender, RoutedEventArgs e)
         {
-            int hour;
-            int minutes;
+            int hour = 0;
+            int minutes = 0;
             string details = eventDetails.Text;
             string duration = eventDuration.Text;
 
-            if (hourSelector.SelectedItem is null)
-                hour = 0;
-            else
+            if (hourSelector.SelectedItem is not null)
                 hour = (int)hourSelector.SelectedItem;
-            if (minuteSelector.SelectedItem is null)
-                minutes = 0;
-            else
+            if (minuteSelector.SelectedItem is not null)
                 minutes = (int)minuteSelector.SelectedItem;
 
             DateTime? selectedDate = new DateTime(DateTime.Now.Year, datePicker.SelectedDate.Value.Month, datePicker.SelectedDate.Value.Day, hour, minutes, 0);
-            string comboBoxSelectedCategory = categoryComboBox.Text;
-            _presenter.ValidateEventFormInputAndUpdate(_calendarItemToUpdate.EventID, details, duration, selectedDate, comboBoxSelectedCategory);
+            Category cat = categoryComboBox.SelectedItem as Category;
+            string comboBoxSelectedCategory = cat.Description;
+            _presenter.ValidateEventFormInputAndUpdate(EventId, details, duration, selectedDate, comboBoxSelectedCategory);
         }
 
         private void BtnClick_AddCategory(object sender, RoutedEventArgs e)
@@ -117,10 +118,7 @@ namespace HomeCalendarWPF
 
         }
 
-        public void AddCategoriesToDropdown(List<Category> categories)
-        {
-            categoryComboBox.ItemsSource = categories;
-        }
+        #endregion
 
     }
 }
